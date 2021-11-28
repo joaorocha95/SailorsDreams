c0e46abbfb60716898f59d2ad13c2eca8f5f9ac5
JoAPSo Pedro Martins Rocha <up201805199@fe.up.pt>
2021-11-28 18:55:17 +0000

    Update A6.sql

--- A6.sql
+++ A6.sql
@@ -1,10 +1,21 @@
+-----------------------------------------
+-- Drop old schema
+-----------------------------------------
 DROP SCHEMA sailorsDream CASCADE;
+
+-----------------------------------------
+-- Types
+-----------------------------------------
 CREATE SCHEMA sailorsDream;
 
 CREATE TYPE sailorsDream.accType AS ENUM ('User', 'Client', 'Seller', 'Admin', 'Support');
 CREATE TYPE sailorsDream.order_status AS ENUM ('In_Negotiation', 'Transaction_Completed', 'Transaction_Failed');
 CREATE TYPE sailorsDream.message_type AS ENUM ('Ticket', 'Order');
 CREATE TYPE sailorsDream.order_type AS ENUM ('Loan', 'Purchase');
+
+-----------------------------------------
+-- Tables
+-----------------------------------------
 
 CREATE TABLE IF NOT EXISTS sailorsDream.Users (
    id SERIAL PRIMARY KEY,
@@ -87,3 +98,40 @@ CREATE TABLE IF NOT EXISTS sailorsDream.Wishlist (
    product INTEGER NOT NULL REFERENCES sailorsDream.Product (id) ON UPDATE CASCADE,
    PRIMARY KEY (userid, product)
 );
+
+
+-----------------------------------------
+-- Populate the database
+-----------------------------------------
+
+   id SERIAL PRIMARY KEY,
+   username TEXT NOT NULL,
+   email TEXT NOT NULL UNIQUE,
+   birthDate DATE NOT NULL,
+   password TEXT NOT NULL,
+   banned BOOLEAN NOT NULL,
+   accType sailorsDream.accType NOT NULL DEFAULT 'User',
+   img TEXT,
+   phone INTEGER NOT NULL UNIQUE
+
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (1,'Thermed','CarlSGossett@dayrep.com','13/02/1946','UTaxe6fieH',FALSE, 'Admin', '', 212288151);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (2,'Selinglese','Virginia J. Crigler','15/12/1974','ohr7ieg2L',FALSE,'Support', '', 212725182);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (3,'Whanterrene','Jeremy T. Tally','13/06/1983','OFK00XCC7OD',FALSE, 'Client', '', 212847851);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (4,'Catelleaden1969','Naja T. Gregersen','12/04/1969','PZJ77DKO2VZ','Cdm',FALSE);
+
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (5,'','Zeph Griffin','rhoncus. Donec est. Nunc ullamcorper,','GUL95ZXR9EX','Praesent',TRUE);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (6,'','Noah Gibson','nunc ac mattis ornare, lectus','TYT71DOD7YN','sollicitudin',TRUE);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (7,'','Aladdin Davidson','nisl elementum purus, accumsan interdum','OFK00XCC7OD','vel',TRUE);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (8,'','Thor Villarreal','Nunc quis arcu vel quam','PZJ77DKO2VZ','Cdm',FALSE);
+
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (9,'','Zeph Griffin','rhoncus. Donec est. Nunc ullamcorper,','GUL95ZXR9EX','Praesent',TRUE);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (10,'','Noah Gibson','nunc ac mattis ornare, lectus','TYT71DOD7YN','sollicitudin',TRUE);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (11,'','Aladdin Davidson','nisl elementum purus, accumsan interdum','OFK00XCC7OD','vel',TRUE);
+INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (12,'','Thor Villarreal','Nunc quis arcu vel quam','PZJ77DKO2VZ','Cdm',FALSE);
+
+
+-- removed for brevity
+
+-----------------------------------------
+-- end
+-----------------------------------------
