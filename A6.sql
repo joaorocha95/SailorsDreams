e1defd30b10239ff69e190ebecf887dd79636c53
Chaca588 <joaojagoncalves10@gmail.com>
2021-11-29 17:13:01 +0000

    fixed A6.sql

--- A6.sql
+++ A6.sql
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS sailorsDream.Review (
    rating_buyer INTEGER NOT NULL,
    rating_seller INTEGER NOT NULL,
    comment TEXT,
-   date DATE DEFAULT now() NOT NULL
+   review_date DATE DEFAULT now() NOT NULL
    CONSTRAINT ratingbuyer_ck CHECK (((rating_buyer > 0) OR (rating_buyer <= 5)))
    CONSTRAINT ratingseller_ck CHECK (((rating_seller > 0) OR (rating_seller <= 5)))
 );
@@ -111,32 +111,34 @@ CREATE TABLE IF NOT EXISTS sailorsDream.Wishlist (
 -- Populate the database
 -----------------------------------------
 
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (1,'Thermed','admin@sailorsdream.com','1946-02-13','UTaxe6fieH',FALSE, 'Admin', '', 212288151);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (2,'Selinglese','selinglese@sailorsdream.com','1946-02-13','ohr7ieg2L',FALSE,'Support', '', 212725182);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (3,'Whanterrene','jemtally83@gmail.com','1946-02-13','OFK00XCC7OD',FALSE, 'Client', '', 212847851);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (4,'Catelleaden1969','najagreen12@gmail.com','1946-02-13','ieSh2riil',FALSE, 'User', '', 212722946);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (1,'Thermed','admin@sailorsdream.com','1946-02-13','UTaxe6fieH',FALSE, 'Admin', '', 212288151);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (2,'Selinglese','selinglese@sailorsdream.com','1946-02-13','ohr7ieg2L',FALSE,'Support', '', 212725182);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (3,'Whanterrene','jemtally83@gmail.com','1946-02-13','OFK00XCC7OD',FALSE, 'Client', '', 212847851);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (4,'Catelleaden1969','najagreen12@gmail.com','1946-02-13','ieSh2riil',FALSE, 'User', '', 212722946);
 
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (5,'Faidelper','kadenn2001@gmail.com','1946-02-13','ohGh3uuG0ei',TRUE, 'User', '', 212735614);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (6,'Sathereend','sunelliot17@gmail.com','1946-02-13','woh2be6Auph1',TRUE,'Seller', '', 212735615);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (7,'Whimpappered52','kaydenstark1952@gmail.com','1946-02-13','sheiJuot3phai',FALSE, 'Client', '', 212441165);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (8,'Difeentle','difeentle@sailorsdream.com','1946-02-13','PZJ77DKO2VZ',FALSE, 'Support', '', 212655776);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (5,'Faidelper','kadenn2001@gmail.com','1946-02-13','ohGh3uuG0ei',TRUE, 'User', '', 212735614);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (6,'Sathereend','sunelliot17@gmail.com','1946-02-13','woh2be6Auph1',TRUE,'Seller', '', 212735615);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (7,'Whimpappered52','kaydenstark1952@gmail.com','1946-02-13','sheiJuot3phai',FALSE, 'Client', '', 212441165);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (8,'Difeentle','difeentle@sailorsdream.com','1946-02-13','PZJ77DKO2VZ',FALSE, 'Support', '', 212655776);
 
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (9,'Painged','paigestew1971@gmail.com','1946-02-13','SahWai0Ie',FALSE, 'Seller', '', 212344626);
-INSERT INTO Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (10,'Eneive','mcdonaldgrey@gmail.com','1946-02-13','eeNgeiHi5',FALSE, 'Client', '', 212349594);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (9,'Painged','paigestew1971@gmail.com','1946-02-13','SahWai0Ie',FALSE, 'Seller', '', 212344626);
+INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (10,'Eneive','mcdonaldgrey@gmail.com','1946-02-13','eeNgeiHi5',FALSE, 'Client', '', 212349594);
 
-INSERT INTO Product (id,seller,productname,description,active,price,pricePerDay) VALUES (1, 6, 'Veleiro', 'Tem uma folha no leme', TRUE, 200.00, NULL);
-INSERT INTO Product (id,seller,productname,description,active,price,pricePerDay) VALUES (2, 6, 'Lancha', 'Rápida e ainda está inteira!', TRUE, 100.00, NULL);
-INSERT INTO Product (id,seller,productname,description,active,price,pricePerDay) VALUES (3, 9, 'Iate', 'Brand new', TRUE,NULL, 75.00);
+INSERT INTO sailorsDream.Product (id,seller,productname,description,active,price,pricePerDay) VALUES (1, 6, 'Veleiro', 'Tem uma folha no leme', TRUE, 200.00, NULL);
+INSERT INTO sailorsDream.Product (id,seller,productname,description,active,price,pricePerDay) VALUES (2, 6, 'Lancha', 'Rápida e ainda está inteira!', TRUE, 100.00, NULL);
+INSERT INTO sailorsDream.Product (id,seller,productname,description,active,price,pricePerDay) VALUES (3, 9, 'Iate', 'Brand new', TRUE,NULL, 75.00);
 
-INSERT INTO Category (product_id, name) VALUES (1, 'Water vehicle');
-
-INSERT INTO Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (1, 1, 3, 'Transaction_Completed', 'Purchase',NULL,NULL, 200.00);
-INSERT INTO Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (1, 1, 10, 'Transaction_Completed', 'Loan', 20/11/2021, 23/11/2021, 75.00);
+INSERT INTO sailorsDream.Category (product_id, name) VALUES (1, 'Water vehicle');
 
 INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (1, 1, 3, 'Transaction_Completed', 'Purchase',NULL,NULL, 200.00);
-INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (2, 3, 10, 'Transaction_Completed', 'Loan', '2021-2-11', '2021-1-11', 75.00);
+INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (2, 1, 10, 'Transaction_Completed', 'Loan', '2021-11-20', '2021-11-23', 75.00);
 
-INSERT INTO sailorsDream.Review (id,orderid,to_user,from_user,rating_buyer,rating_seller,comment) VALUES (1,1,3,6,5,5, 'Very cheap and fast!');
+INSERT INTO sailorsDream.Review (id, orderid, to_user, from_user, rating_buyer, rating_seller, comment) VALUES (1, 1, 2, 4, 4, 4, NULL);
+
+INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (3, 1, 3, 'Transaction_Completed', 'Purchase',NULL,NULL, 200.00);
+INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (4, 3, 10, 'Transaction_Completed', 'Loan', '2021-2-11', '2021-1-11', 75.00);
+
+INSERT INTO sailorsDream.Review (id,orderid,to_user,from_user,rating_buyer,rating_seller,comment) VALUES (2,1,3,6,5,5, 'Very cheap and fast!');
 
 -- removed for brevity
 
