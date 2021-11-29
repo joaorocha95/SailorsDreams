2a229ebd7eb2111742b4d6e5bde164a07bf1dfa8
Chaca588 <joaojagoncalves10@gmail.com>
2021-11-29 22:49:55 +0000

    Fix identation

--- sql/A6.sql
+++ sql/A6.sql
@@ -3,7 +3,7 @@
 -----------------------------------------
 
 -----------------------------------
-	User Inserts
+--  	User Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (1,'Thermed','admin@sailorsdream.com','1946-02-13','UTaxe6fieH',FALSE, 'Admin', '', 212288151);
 INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (2,'Selinglese','selinglese@sailorsdream.com','1946-02-13','ohr7ieg2L',FALSE,'Support', '', 212725182);
@@ -17,49 +17,49 @@ INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accT
 INSERT INTO sailorsDream.Users (id,username,email,birthDate,password,banned,accType,img,phone) VALUES (10,'Eneive','mcdonaldgrey@gmail.com','1946-02-13','eeNgeiHi5',FALSE, 'Client', '', 212349594);
 
 -----------------------------------
-	Product Inserts
+--  	Product Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Product (id,seller,productname,description,active,price,pricePerDay) VALUES (1, 6, 'Veleiro', 'Tem uma folha no leme', TRUE, 200.00, NULL);
 INSERT INTO sailorsDream.Product (id,seller,productname,description,active,price,pricePerDay) VALUES (2, 6, 'Lancha', 'Rápida e ainda está inteira!', TRUE, 100.00, NULL);
 INSERT INTO sailorsDream.Product (id,seller,productname,description,active,price,pricePerDay) VALUES (3, 9, 'Iate', 'Brand new', TRUE,NULL, 75.00);
 
 -----------------------------------
-	Order Inserts
+--  	Order Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (1, 1, 3, 'Transaction_Completed', 'Purchase',NULL,NULL, 200.00);
 INSERT INTO sailorsDream.Order (id,product,client,order_status,order_type,loan_start,loan_end, total_price) VALUES (2, 1, 10, 'Transaction_Completed', 'Loan', '2021-11-20', '2021-11-23', 75.00);
 
 -----------------------------------
-	Review Inserts
+--  	Review Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Review (id, orderid, to_user, from_user, rating, comment) VALUES (1, 1, 2, 4, 4, NULL);
 
 -----------------------------------
-	Addresses Inserts
+--  	Addresses Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Addresses (userid,addr,city,country,zipcode) VALUES (9,'4152 Woodside Circle', 'Pensacola, FL', 'United States of America', '19801');
 INSERT INTO sailorsDream.Addresses (userid,addr,city,country,zipcode) VALUES (6,'3452 Oakmound Drive', 'Chicago, IL', 'United States of America', '60626');
 
 -----------------------------------
-	Category Inserts
+--   	Category Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Category (product_id, name) VALUES (1, 'Water vehicle');
 INSERT INTO sailorsDream.Category (product_id, name) VALUES (3, 'Luxurious vehicle');
 
 -----------------------------------
-	Ticket Inserts
+--  	Ticket Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Ticket(id, userid, support, title) VALUES (1, 3, 2, 'Problem with my loan!');
 INSERT INTO sailorsDream.Ticket(id, userid, support, title) VALUES (2, 6, 8, 'Cant see any product');
 
 -----------------------------------
-	Message Inserts
+--  	Message Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Message(id, message_type, associated_order, associated_ticket, message, date) VALUES (1, 'Ticket', NULL, 2, 'Hey, can you fix the product pages please?', );
 INSERT INTO sailorsDream.Message(id, message_type, associated_order, associated_ticket, message, date) VALUES (2, 'Order', 2, NULL, 'Oi, onde estaciono o iate?', );
 
 -----------------------------------
-	Wishlist Inserts
+--  	Wishlist Inserts
 -----------------------------------
 INSERT INTO sailorsDream.Wishlist(userid, product) VALUES (3, 2);
 INSERT INTO sailorsDream.Wishlist(userid, product) VALUES (3, 3);
--- sql/index.sql
+++ sql/index.sql
@@ -1,7 +1,13 @@
-CREATE INDEX message_order ON sailorsDream.Message USING hash (associated_order);
-CLUSTER sailorsDream.Message USING message_order
+CREATE INDEX message_order
+    ON sailorsDream.Message
+    USING hash (associated_order);
+    CLUSTER sailorsDream.Message USING message_order
 
-CREATE INDEX category_name ON sailorsDream.Category USING hash (name);
-CLUSTER sailorsDream.Category USING category_name
+CREATE INDEX category_name
+    ON sailorsDream.Category
+    USING hash (name);
+    CLUSTER sailorsDream.Category USING category_name
 
-CREATE INDEX product_productname ON sailorsDream.Product USING hash (productname)
+CREATE INDEX product_productname
+    ON sailorsDream.Product
+    USING hash (productname)
