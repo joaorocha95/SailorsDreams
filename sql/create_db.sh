315e9a9781ddb60c5aa6035de4a4aab0edaa2582
up201805137 <chaca@jota.localdomain>
2021-12-08 17:49:13 +0000

    create_db.sh now asks for password

--- sql/create_db.sh
+++ sql/create_db.sh
@@ -3,6 +3,9 @@
 echo "Initializing Postgres table creation"
 echo "Wish to proceed? [Y/n]"
 read choice
+echo "Enter lbaw2182 password's:"
+read password
+export PGPASSWORD=password
 if [ $choice = "Y" ] || [ $choice = "y" ] || [ $choice = "yes" ] ; then
     echo "Reunning..."
     command psql --host=db.fe.up.pt --dbname=lbaw2182 --user=lbaw2182 -f schema.sql
@@ -10,7 +13,9 @@ if [ $choice = "Y" ] || [ $choice = "y" ] || [ $choice = "yes" ] ; then
     command psql --host=db.fe.up.pt --dbname=lbaw2182 --user=lbaw2182 -f index.sql
     command psql --host=db.fe.up.pt --dbname=lbaw2182 --user=lbaw2182 -f populate.sql
     else
+	export PGPASSWORD=' '
         echo "Invalid Option"
         exit
 fi
+export PGPASSWORD=' '
 echo "Completed"
